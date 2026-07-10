42d93d763ed071b1a145fbb8ea1cf02c618b9de5
Ivan-jauregui <ivanjaregui79@gmail.com>
2026-07-10 13:40:17 -0300

    Added mobile styling, functional form, and WhatsApp link

--- src/app/features/landing/components/cta/cta.component.ts
+++ src/app/features/landing/components/cta/cta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'cta',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './cta.component.css'
 })
 export class CtaComponent {
+    sendEmail(event:Event){
+    event.preventDefault();
 
+      emailjs.sendForm('TU_SERVICE_ID', 'TU_TEMPLATE_ID', event.target as HTMLFormElement, 'TU_PUBLIC_KEY')
+      .then(() => {
+        alert('¡Correo enviado con éxito!');
+      }, (error) => {
+        console.error('Error al enviar:', error);
+      });
+  }
 }
--- src/app/features/landing/components/uvp/uvp.component.ts
+++ src/app/features/landing/components/uvp/uvp.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'uvp',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass ],
   templateUrl: './uvp.component.html',
   styleUrl: './uvp.component.css'
 })
 export class UvpComponent {
+statements: string[] = [
+    `"Por cada componente electrónico que recuperamos, mitigamos el impacto ambiental y desarrollamos <span class="text-green-700 font-bold">tecnología funcional a medida</span>. No solo protegemos el planeta: transformamos activos obsoletos en soluciones personalizadas de alto valor añadido."`,
+    
+    `"A través del <span class="text-green-700 font-bold">Upcycling Electrónico</span>, diseñamos y fabricamos productos tecnológicos a pedido adaptados a requerimientos operativos corporativos, reduciendo drásticamente costos y huella de carbono."`,
+    
+    `"Mediante nuestro <span class="text-green-700 font-bold">Software Eco-Inteligente</span>, desarrollamos plataformas digitales especializadas que permiten a las organizaciones medir, gestionar y optimizar su impacto ecológico real."`
+  ];
 
+  currentIndex: number = 0;
+  private intervalId: any;
+  isVisible: boolean = true;
+
+  ngOnInit(): void {
+    // Cambia el texto automáticamente cada 5 segundos
+    this.intervalId = setInterval(() => {
+      this.rotateText();
+    }, 5000);
+  }
+
+  rotateText(): void {
+    // Desvanece el texto actual (fade out)
+    this.isVisible = false;
+
+    setTimeout(() => {
+      // Cambia al siguiente texto
+      this.currentIndex = (this.currentIndex + 1) % this.statements.length;
+      // Vuelve a mostrarlo (fade in)
+      this.isVisible = true;
+    }, 300); // 300ms coincide con la duración de la transición en el HTML
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
